e7d520aa34b956542a4a861720409d4417833728
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-23 15:41:17 -0500

    Explorers.json, Reader.js, Reader.test.js y server.js

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,7 @@
+const Reader = require("./../../../lib/utils/Reader")
+describe("Unit Test para la clase Reader",()=>{
+    test("Lectura de un path",()=>{
+        const explorers = Reader.readJsonFile("explorers.json")
+        expect(explorers).toBeDefined()
+    })
+})

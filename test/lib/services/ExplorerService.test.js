da1f8f57b253ef87649a42e314b9fce20771aabf
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-23 21:02:25 -0500

    función: getChekedEmails; clase: ExplorerService

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -4,4 +4,10 @@ describe("Unit Testing Clase ExplorerService",()=>{
         const explorers = ExplorerService.getAllExplorers()
         expect(explorers).toBeDefined()
     })
+    test("Metodo getCheckedEmails, regresa los emails de los explorers que tienen certificacion",()=>{
+        const explorers = ExplorerService.getCheckedEmails()
+        const check = true
+        explorers.forEach((explorer)=>{if(explorer.haveCertification==false){check = false}})
+        expect(check).toBe(true)
+    })
 })

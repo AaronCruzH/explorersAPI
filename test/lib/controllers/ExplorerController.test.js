4decfe4c59df6486d6bf27aeda28bdd7f2ebcd42
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-23 21:39:33 -0500

    función: filterCheckedEmails clase: ExplorerController

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -4,4 +4,10 @@ describe("Unit Testing ExplorerController",()=>{
         const explorers = ExplorerController.filterAllExplorers()
         expect(explorers).toBeDefined()
     })
+    test("filterCheckedEmails, devuelve la lista explorers con certificación",()=>{
+        const explorers = ExplorerController.filterCheckedEmails()
+        const check = true
+        explorers.forEach((explorer)=>{if(explorer.haveCertification==false){check = false}})
+        expect(check).toBe(true)
+    })
 })
